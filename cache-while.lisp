ed7fd36b31e5c28016765bd250707bd71dab14ef
Charles Jackson <cbj170230@utdallas.edu>
2021-06-13 23:43:35 -0500

    Whitespace

--- cache-while.lisp
+++ cache-while.lisp
@@ -4,7 +4,7 @@
   (let ((values (gensym))
         (eqs (gensym))
         (mspots (loop for i from 1 to (length eqs-values)
-                     collect i))
+                      collect i))
         (result 0)
         (mvalues (map 'list 'second eqs-values))
         (meqs (map 'list 'first eqs-values))
@@ -27,22 +27,21 @@
                   (setf (gethash ',cache ,calculated-place)
                         (make-array ,(+ 1 (length mspots))
                                     :initial-element nil)))))
-            (if (notevery
-                 (lambda (val1 test val2)
-                   (funcall test val1 val2))
-                 (map 'list (lambda (spot)
-                              (elt ,cache spot))
-                      ,spots)
-                 ,eqs
-                 ,values)
-                (progn
-                  (mapc
-                   (lambda (spot value)
-                     (setf (elt ,cache spot)
-                           value))
-                   ,spots
-                   ,values)
-                  (setf (elt ,cache ,result)
-                        (progn
-                          ,@body)))
-                (elt ,cache ,result)))))
+       (if (notevery
+            (lambda (val1 test val2)
+              (funcall test val1 val2))
+            (map 'list (lambda (spot)
+                         (elt ,cache spot))
+                 ,spots)
+            ,eqs
+            ,values)
+           (progn
+             (mapc
+              (lambda (spot value)
+                (setf (elt ,cache spot)
+                      value))
+              ,spots
+              ,values)
+             (setf (elt ,cache ,result)
+                   (progn ,@body)))
+           (elt ,cache ,result)))))
