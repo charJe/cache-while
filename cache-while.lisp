4bd5fba7f2b3d47a1140f6d8cf4109b6dbebd525
Charles Jackson <cbj170230@utdallas.edu>
2021-06-13 23:36:23 -0500

    Make sure place is only evaluated once

--- cache-while.lisp
+++ cache-while.lisp
@@ -10,38 +10,37 @@
         (meqs (map 'list 'first eqs-values))
         (cache (gensym "CACHE"))
         (calculated-place (gensym)))
-    `(progn
-       (unless ,(if (symbolp place)
-                    `(and (boundp ',place)
-                          ,place)
-                    place)
-         ;; PLACE is table of arrays
-         (setf ,place (make-hash-table)))
-       (let* ((,values (list ,@mvalues))
-              (,eqs ',meqs)
-              (,calculated-place ,place)
-              ;; CACHE is array
-              (,cache
-                (or (gethash ',cache ,calculated-place)
-                    (setf (gethash ',cache ,calculated-place)
-                          (make-array ,(+ 1 (length spots))
+    `(let* ((,values (list ,@mvalues))
+            (,eqs ',meqs)
+            (,calculated-place
+              (or ,(if (symbolp place)
+                       `(and (boundp ',place)
+                             ,place)
+                       place)
+                  ;; PLACE is table of arrays
+                  (setf ,place (make-hash-table))))
+            ;; CACHE is array
+            (,cache
+              (or (gethash ',cache ,calculated-place)
+                  (setf (gethash ',cache ,calculated-place)
+                        (make-array ,(+ 1 (length spots))
                                     :initial-element nil)))))
-         (if (notevery
-              (lambda (val1 test val2)
-                (funcall test val1 val2))
-              (map 'list (lambda (spot)
-                           (elt ,cache spot))
-                   ',spots)
-              ,eqs
-              ,values)
-           (progn
-             (mapc
-              (lambda (spot value)
-                (setf (elt ,cache spot)
-                      value))
-              ',spots
-              ,values)
-             (setf (elt ,cache ,result)
-                   (progn
-                     ,@body)))
-           (elt ,cache ,result))))))
+            (if (notevery
+                 (lambda (val1 test val2)
+                   (funcall test val1 val2))
+                 (map 'list (lambda (spot)
+                              (elt ,cache spot))
+                      ',spots)
+                 ,eqs
+                 ,values)
+                (progn
+                  (mapc
+                   (lambda (spot value)
+                     (setf (elt ,cache spot)
+                           value))
+                   ',spots
+                   ,values)
+                  (setf (elt ,cache ,result)
+                        (progn
+                          ,@body)))
+                (elt ,cache ,result)))))
